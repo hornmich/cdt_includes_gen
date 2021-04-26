15f249501cdba88cb0674b23bae3b6c5a321e6a0
Mariwan <mariwan.jalal@gmail.com>
2021-04-26 08:48:15 +0200

    Update gen_cdt_settings.py
    Python2 - 3 Conversion .. All print commands need new Parentheses

--- gen_cdt_settings.py
+++ gen_cdt_settings.py
@@ -14,40 +14,45 @@
 """
 
 if __name__ == "__main__":
-    import argparse
-    import cig
-    import os
-
-    parser = argparse.ArgumentParser(description='Generate includes settings for Eclipse CDT project.')
-    parser.add_argument('out_file', help='specify the output file filename')
-    parser.add_argument('-v', '--verbose', help='turn on verbose mode', action="store_true")
-    parser.add_argument('--ignore-c', help='ignore C header files', action="store_true")
-    parser.add_argument('--ignore-cpp', help='ignore C++ header files', action="store_true")
-    args = parser.parse_args()   
-
-    if args.ignore_c :
-        print("Skipping C header files.")
-        hFolders = set()
-    else:
-        print("Searching for C header files.")
-        hFolders = cig.folders(os.getcwd(), ['.h']) 
-    if args.ignore_cpp :
-        print("Skipping C++ header files.")
-        hppFolders = set()
-    else:
-        print("Searching for C++ header files.")
-        hppFolders = cig.folders(os.getcwd(), ['.hpp']) 
-
-    if args.verbose:
-        print('C includes paths({0}):').format(len(hFolders))
-        for folder in hFolders:
-            print('\t{0}').format(folder)
-        print('C++ includes paths({0}):').format(len(hppFolders))
-        for folder in hppFolders:
-            print('\t{0}').format(folder)
-
-    print('Writting settings file to {0}.').format(args.out_file)
-    cig.generateXML(args.out_file, hFolders, hppFolders)
-    print('Done.')
-
-
+    try:
+        import argparse
+        import cig
+        import os,sys
+    
+        parser = argparse.ArgumentParser(description='Generate includes settings for Eclipse CDT project.')
+        parser.add_argument('out_file', help='specify the output file filename')
+        parser.add_argument('-v', '--verbose', help='turn on verbose mode', action="store_true")
+        parser.add_argument('--ignore-c', help='ignore C header files', action="store_true")
+        parser.add_argument('--ignore-cpp', help='ignore C++ header files', action="store_true")
+        args = parser.parse_args()   
+    
+        if args.ignore_c :
+            print("Skipping C header files.")
+            hFolders = set()
+        else:
+            print("Searching for C header files.")
+            hFolders = cig.folders(os.getcwd(), ['.h']) 
+        if args.ignore_cpp :
+            print("Skipping C++ header files.")
+            hppFolders = set()
+        else:
+            print("Searching for C++ header files.")
+            hppFolders = cig.folders(os.getcwd(), ['.hpp']) 
+    
+        if args.verbose:
+            print(('C includes paths({0}):').format(len(hFolders)))
+            for folder in hFolders:
+                print(('\t{0}').format(folder))
+            print(('C++ includes paths({0}):').format(len(hppFolders)))
+            for folder in hppFolders:
+                print(('\t{0}').format(folder))
+    
+        print(('Writting settings file to {0}.').format(args.out_file))
+        cig.generateXML(args.out_file, hFolders, hppFolders)
+        print('Done.')
+    except Exception as err:
+        App.Console.PrintError("'Part::Box' Failed. "
+                               "{err}\n".format(err=str(err)))
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
